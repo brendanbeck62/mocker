1af5aeca44bd3e5b1b645a08370a653a6cf695bc
Brendan Beck <brendan.beck62@gmail.com>
2020-09-13 13:57:15 -0700

    finished mocker! I willl continue updating it with features as I learn new things.

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# mocker
-
+# Mocker
 A docker clone
+
+This is based on the [How to create your own containers](https://cesarvr.io/post/2018-05-22-create-containers/) blog post.
+Full disclosure, I attempted each section by myself before refering to his source code.
+
+This was done purely as a learning experience and is my work supplimented largly by Cesar Valdez's work.
+
+## Run
+`make -s && sudo ./container`
+
+The first time ran, you need to download and extract the alpine os.
+I did not think making the user pull the entire OS along with the container source was a good idea, although I would love feedback on this (open an issue if you have feedback!)
+```
+mkdir root && cd root
+curl -Ol http://nl.alpinelinux.org/alpine/v3.7/releases/x86_64/alpine-minirootfs-3.7.0-x86_64.tar.gz
+tar -xvf alpine-minirootfs-3.7.0_rc1-x86_64.tar.gz
+rm alpine-minirootfs-3.7.0_rc1-x86_64.tar.gz
+```
+
+## Implentation details
+
+### Chroot and Alpine
+The container will use chroot to isolate the filesystem, which means we will lose linux commands inside the container. To combat this, we will run an alpine instance inside the container as our new root.
+
+### Limited Process Creation
+I thought it would be an interesting exercize to limit the number of proccesses that could be created by the container.
+Currently it is limitted to 5 process at a time, although 2 of them are taken up by the actual container, and bin/sh. So the user gets 3 processes to play with. This can be changed by changing the NUM_PROCESSES_ALLOWED def.
--- container.cc
+++ container.cc
@@ -1,18 +1,59 @@
 #include <iostream>
 #include <sched.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <fstream>
+
+#define CGROUP_FOLDER "/sys/fs/cgroup/pids/container/"
+#define concat(a,b) (a"" b)
+#define NUM_PROCESSES_ALLOWED "5"
 
 /**
- * @brief called from the jail method, spawns a shell.
- * called by: run("/bin/sh");
+ * @brief custom try block
  *
- * @param name the command to exec
+ * @param status
+ * @param msg
+ * @return int
  */
-int run(const char *name) {
-    char *_args[] = {(char *)name, (char *)0 };
-    return execvp(name, _args);
+int TRY(int status, const char *msg) {
+    if(status == -1) {
+        perror(msg);
+        exit(EXIT_FAILURE);
+    }
+    return status;
+}
+
+/**
+ * @brief writes the process id to cgroup.procs
+ *
+ * @param path
+ * @param value
+ */
+void write_rule(const char* path, const char* value) {
+    int fp = open(path, O_WRONLY | O_APPEND );
+    write(fp, value, strlen(value));
+    close(fp);
+}
+
+/**
+ * @brief use cgroups to limit the amount of processes we can create inside our container
+ * the control group called pids controller can be used to limit the amount of
+ * times a process can replicate itself, for example using fork or clone.
+ *
+ */
+void limitProcessCreation() {
+    mkdir( CGROUP_FOLDER, S_IRUSR | S_IWUSR);  // Read & Write
+    const char* pid  = std::to_string(getpid()).c_str();
+
+    write_rule(concat(CGROUP_FOLDER, "pids.max"), NUM_PROCESSES_ALLOWED);
+    write_rule(concat(CGROUP_FOLDER, "notify_on_release"), "1");
+    write_rule(concat(CGROUP_FOLDER, "cgroup.procs"), pid);
 }
 
 /**
@@ -33,6 +74,15 @@ char* stack_memory() {
 }
 
 /**
+ * @brief Set the Host Name of the container
+ *
+ * @param hostname
+ */
+void setHostName(std::string hostname) {
+    sethostname(hostname.c_str(), hostname.size());
+}
+
+/**
  * @brief handles the env variables of the new container
  *
  */
@@ -40,6 +90,17 @@ void setup_variables() {
     clearenv();
     setenv("TERM", "xterm-256color", 0);
     setenv("PATH", "/bin/:/sbin/:usr/bin:/usr/sbin", 0);
+}
+
+/**
+ * @brief called from the jail method, spawns a shell.
+ * called by: run("/bin/sh");
+ *
+ * @param name the command to exec
+ */
+int run(const char *name) {
+    char *_args[] = {(char *)name, (char *)0 };
+    return execvp(name, _args);
 }
 
 /**
@@ -53,34 +114,50 @@ void setup_root(const char* folder){
 }
 
 /**
+ * @brief creates the child process
+ * creates a new “generic type” called Function which will morph into a C function,
+ * then we pass the function to clone, also we pass the flags as an integer.
+ * @tparam Function
+ * @param function - in this case, it is used to call jail
+ * @param flags - the falgs to send clone
+ */
+template <typename Function>
+void clone_process(Function&& function, int flags){
+    auto pid = clone(function, stack_memory(), flags, 0);
+
+    wait(nullptr);
+}
+
+/**
  * @brief the child process that is called from clone
  *
  * @param args any optional args sent from clone
  * @return int EXIT_STATUS
  */
+#define lambda(fn_body) [](void *args) ->int { fn_body; };
 int jail(void *args) {
-    printf("child process: %d", getpid());
 
+    // Limits the number of process that can be spawned in the continer to 5
+    // but its really more like 3, because container and sh take up 2 of them
+    limitProcessCreation();
+    printf("child pid: %d\n", getpid());
+    setHostName("my-container");
     setup_variables();
     setup_root("./root");
+    mount("proc", "/proc", "proc", 0, 0);
 
-    run("/bin/sh");
+    // lambda "runnable" that is passed into the clone_process function
+    auto runnable = lambda(run("/bin/sh"))
+    clone_process(runnable, SIGCHLD);
+
+    umount("/proc");
     return EXIT_SUCCESS;
 }
 
 int main(int argc, char** argv) {
-    printf("parent process: %d\n", getpid());
 
-    // creates the child process
-    // NOTE: sementics of clone differ from fork() in that the child process starts
-    // by calling the function defined as the first parameter
-    // CLONE_NEWPID -
-    // CLONE_NEWUTS -
-    // SIGCHLD - flag that tells the process to emit a signal when finished
-    // 0 - parameter we send to jail()
-
-    clone(jail, stack_memory(), CLONE_NEWPID | CLONE_NEWUTS | SIGCHLD, 0);
-    wait(nullptr);
+    printf("parent pid: %d\n", getpid());
+    clone_process(jail, CLONE_NEWPID | CLONE_NEWUTS | SIGCHLD );
 
     return EXIT_SUCCESS;
 }
